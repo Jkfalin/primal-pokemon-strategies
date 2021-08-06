35460dfa667aa6761deeb4add1939cb6b04ac4cd
Jkfalin <jakob.falin@gmail.com>
2021-08-05 18:48:56 -0600

    ugh

--- copyright.js
+++ copyright.js
@@ -1,9 +1,12 @@
-var date = new Date()
-var m = date.getMonth()
-var d = date.getDay()
-var y = date.getFullYear()
 
-var copyright = document.querySelector("#copyright")
+let copyright = document.querySelector("#copyright");
 
-copyright.textContent = 'Copyright ' + m + '-' + d + '-'+ y
+let now = moment();
+let dateString = now.format("MM-DD-YYYY"); 
+
+
+
+
+copyright.textContent = dateString;
+
 
